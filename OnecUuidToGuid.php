c8d613f84db2c21f1484fd05d4adb9ed560f7b8f
Alex <39405237+iamguruman@users.noreply.github.com>
2021-10-11 08:12:48 +0300

    Create OnecUuidToGuid.php

---
+++ OnecUuidToGuid.php
@@ -0,0 +1,11 @@
+<?php
+
+public static function OnecUuidToGuid(){
+
+
+      Возврат Прав(UUID, 8) + "-" + Сред(UUID, 21, 4) + "-" + Сред(UUID, 17, 4) + "-" + Лев(UUID, 4) + "-" + Сред(UUID, 5, 12);
+
+
+}
+    
+?>
